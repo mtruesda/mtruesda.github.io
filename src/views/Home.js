e3dde9feeb350094ce7cd3ccc8487dec5807720b
mtruesda <mtruesda@gmail.com>
2025-03-29 01:34:06 -0400

    preparing site for content additions and completion

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,7 @@
 import '../css/Home.css';
 import React, { useEffect } from 'react';
+import denmark from '../images/KateMyronDenmark.png';
+import germany from '../images/KateMyronGermany.png';
 
 
 function Home() {
@@ -13,6 +15,10 @@ function Home() {
         <div id='main-content'>
             <h1>Home</h1>
             
+            <img src="" alt="Me sitting in an H-60"></img>
+            <img src={denmark} alt="Katelyn and I at a castle in Denmark" className="denmark-image"></img>
+            <img src={germany} alt="Katelyn and I at a castle in Germany" className="germany-image"></img>
+
             <fieldset class="container">
                 <legend>Connect with me!</legend>
             </fieldset>
